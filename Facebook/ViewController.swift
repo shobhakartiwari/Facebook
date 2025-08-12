1186d4dafb438bdaa38ef023d015398f873e6de2
Shobhakar Tiwari <st.shubh.tiwari@gmail.com>
2025-08-12 16:15:15 -0500

    Initial Commit

---
+++ Facebook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Facebook
+//
+//  Created by Shobhakar Tiwari on 8/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FacebookUITests/FacebookUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FacebookUITestsLaunchTests.swift
+//  FacebookUITests
+//
+//  Created by Shobhakar Tiwari on 8/12/25.
+//
+
+import XCTest
+
+final class FacebookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
